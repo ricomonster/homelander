faed5d863c6f6072f856261f23263d9768407b16
ricomonster <2063313+ricomonster@users.noreply.github.com>
2026-09-23 16:34:58 +0800

    chore(nvim): update lensline configuration to use version key

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -597,7 +597,7 @@ do
   end, { desc = 'Toggle file explorer' })
 
   vim.pack.add {
-    { src = gh 'oribarilan/lensline.nvim', tag = 'v2.1.0' },
+    { src = gh 'oribarilan/lensline.nvim', version = 'v2.1.0' },
   }
   require('lensline').setup()
 end
