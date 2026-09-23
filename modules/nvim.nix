e036b1b085bdeb06625c7a84e3e10e812e3f86b8
ricomonster <2063313+ricomonster@users.noreply.github.com>
2026-09-23 15:47:41 +0800

    feat(nvim): disable python3 support

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -1,11 +1,11 @@
 { ... }:
-
 {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     viAlias = true;
     vimAlias = true;
+    withPython3 = false;
   };
 
   xdg.configFile."nvim".source = ./nvim;
